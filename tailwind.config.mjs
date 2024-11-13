d43dd4722ced55fd26b597aa4dbf7c6eb0c580aa
manpreet <manpreetrahal27@gmail.com>
2024-11-12 20:00:27 -0700

    a decent start

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#151515",
+      },
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+        sans: [
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
